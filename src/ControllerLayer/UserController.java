478cb008038c334b18358c914c60a07691be114e
Gabi Moldovan <moldovangabi91@yahoo.com>
2024-11-13 12:49:37 +0200

    Added Javadocs

--- src/ControllerLayer/UserController.java
+++ src/ControllerLayer/UserController.java
@@ -1,4 +1,5 @@
 package ControllerLayer;
+
 import ModelLayer.Event;
 import RepoLayerInterface.*;
 import ModelLayer.Bet;
@@ -7,36 +8,74 @@ import ModelLayer.PlayerBet;
 import ServiceLayer.BetService;
 import ServiceLayer.UserService;
 
+/**
+ * Controller class responsible for handling user-related actions such as viewing players, updating admin access levels,
+ * viewing player bets, and creating new bet events.
+ * It acts as an intermediary between the service layer and the user interface (or external input).
+ */
 public class UserController {
+    /**
+     * Service for handling betting-related operations.
+     */
     private final BetService betService;
+
+    /**
+     * Service for handling user-related operations, including players and admins.
+     */
     private final UserService userService;
 
+    /**
+     * Constructs a UserController with the specified BetService and UserService.
+     *
+     * @param betService the BetService used for handling bets and events
+     * @param userService the UserService used for handling user operations
+     */
     public UserController(BetService betService, UserService userService) {
         this.betService = betService;
         this.userService = userService;
     }
 
-    public void viewPlayers(int player_id){
+    /**
+     * Displays all available players in the system.
+     *
+     * @param player_id the ID of the player (not used in this method, but kept for potential future use)
+     */
+    public void viewPlayers(int player_id) {
         StringBuilder output = new StringBuilder("Available Players: ");
         userService.getAllPlayers().forEach(player -> output.append(player.toString()).append("/n"));
         System.out.println(output);
     }
 
-    public void updateAdmin(int admin_id,int accesslevel){
-        userService.updateAccesLevelAdmin(admin_id,accesslevel);
-        System.out.println("Admin"+admin_id+"updated to acces Level:"+accesslevel);
+    /**
+     * Updates the access level of an admin.
+     *
+     * @param admin_id the ID of the admin to update
+     * @param accesslevel the new access level for the admin
+     */
+    public void updateAdmin(int admin_id, int accesslevel) {
+        userService.updateAccesLevelAdmin(admin_id, accesslevel);
+        System.out.println("Admin " + admin_id + " updated to access level: " + accesslevel);
     }
 
-    public void viewPlayerBets(Integer player_id){
+    /**
+     * Displays all available bets for a specific player.
+     *
+     * @param player_id the ID of the player whose bets are to be displayed
+     */
+    public void viewPlayerBets(Integer player_id) {
         StringBuilder output = new StringBuilder("Available Bets: ");
-        betService.getAvailableBets().forEach(player -> output.append(player.toString()).append("/n"));
+        betService.getAvailableBets().forEach(bet -> output.append(bet.toString()).append("/n"));
         System.out.println(output);
     }
 
-    public void new_betEvent(String name, String eventType){
-        betService.addEvent(name,eventType);
-        System.out.println("New Bet EventType for"+eventType+name+"will be available for betting  soon!: ");
+    /**
+     * Creates a new bet event and adds it to the system.
+     *
+     * @param name the name of the new event
+     * @param eventType the type of the event (e.g., sports, game, etc.)
+     */
+    public void new_betEvent(String name, String eventType) {
+        betService.addEvent(name, eventType);
+        System.out.println("New Bet Event for " + eventType + ": " + name + " will be available for betting soon!");
     }
-
 }
-
--- src/ModelLayer/BasketOdds.java
+++ src/ModelLayer/BasketOdds.java
@@ -1,15 +1,33 @@
 package ModelLayer;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents the odds related to a basketball event.
+ * This class extends the Odds class and includes additional fields specific to basketball.
+ */
 public class BasketOdds extends Odds {
+
+    /** The odds for a draw in the game. */
     private int odd_draw;
-    private Map<String,Double> most_3p;
-    private Map<String,Double> most_reb;
 
+    /** A map of players' names to their odds of making the most 3-point shots. */
+    private Map<String, Double> most_3p;
 
+    /** A map of players' names to their odds of collecting the most rebounds. */
+    private Map<String, Double> most_reb;
+
+    /**
+     * Constructs a new BasketOdds object.
+     *
+     * @param event_id  The unique identifier for the event.
+     * @param odd_value A list of odds values associated with the event.
+     * @param eventType The type of event the odds are associated with.
+     * @param odd_draw  The odds for a draw in the game.
+     * @param most_3p   A map of players to their odds of making the most 3-point shots.
+     * @param most_reb  A map of players to their odds of collecting the most rebounds.
+     */
     public BasketOdds(int event_id, List<Double> odd_value, String eventType, int odd_draw, Map<String, Double> most_3p, Map<String, Double> most_reb) {
         super(event_id, odd_value, eventType);
         this.odd_draw = odd_draw;
@@ -17,26 +35,56 @@ public class BasketOdds extends Odds {
         this.most_reb = most_reb;
     }
 
+    /**
+     * Gets the odds for a draw in the game.
+     *
+     * @return The draw odds.
+     */
     public int getOdd_draw() {
         return odd_draw;
     }
 
-    public Map<String, Double> getMost_3p() {
-        return most_3p;
-    }
-
-    public Map<String, Double> getMost_reb() {
-        return most_reb;
-    }
-
+    /**
+     * Sets the odds for a draw in the game.
+     *
+     * @param odd_draw The draw odds to set.
+     */
     public void setOdd_draw(int odd_draw) {
         this.odd_draw = odd_draw;
     }
 
+    /**
+     * Gets the map of players to their odds of making the most 3-point shots.
+     *
+     * @return A map of players to their 3-point odds.
+     */
+    public Map<String, Double> getMost_3p() {
+        return most_3p;
+    }
+
+    /**
+     * Sets the map of players to their odds of making the most 3-point shots.
+     *
+     * @param most_3p The map of players to their 3-point odds.
+     */
     public void setMost_3p(Map<String, Double> most_3p) {
         this.most_3p = most_3p;
     }
 
+    /**
+     * Gets the map of players to their odds of collecting the most rebounds.
+     *
+     * @return A map of players to their rebound odds.
+     */
+    public Map<String, Double> getMost_reb() {
+        return most_reb;
+    }
+
+    /**
+     * Sets the map of players to their odds of collecting the most rebounds.
+     *
+     * @param most_reb The map of players to their rebound odds.
+     */
     public void setMost_reb(Map<String, Double> most_reb) {
         this.most_reb = most_reb;
     }
